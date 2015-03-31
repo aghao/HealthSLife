00cdca54da8809ab466b6194d2851ad09b249ce1
zfy930627 <290525062@qq.com>
2015-03-31 11:44:36 +0800

    add layout and cameraview

--- src/com/healthlife/activity/HeartRate.java
+++ src/com/healthlife/activity/HeartRate.java
@@ -1,5 +1,49 @@
 package com.healthlife.activity;
 
-public class HeartRate {
+import com.healthslife.R;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.SurfaceView;
+
+public class HeartRate extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.heartrate_main);
+	}
+
+	@Override
+	protected void onDestroy() {
+		// TODO Auto-generated method stub
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+	}
+	
+	//自定义相机视图  开启相机，预览图像
+	class CameraView extends SurfaceView {
+		public CameraView(Context context) {
+			super(context);
+		}
+	}
 
 }
